a493866396e2ec1b6b38fd6a2848e85a91e4400a
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-04-06 04:25:19 -0400

    Added the add_task_screen, which contains the widget builder pf the showModalBottomSheet

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 
 // Screens:
+import 'package:todoey_flutter/screens/add_task_screen.dart';
 
 // Components:
 import 'package:todoey_flutter/components/tasks_list.dart';
@@ -21,85 +22,6 @@ class TasksScreen extends StatelessWidget {
       duration: Duration(milliseconds: 500),
       // curve: Curves.fastOutSlowIn,
       curve: Curves.easeOut,
-    );
-  }
-
-  Widget buildModalBottomSheet(BuildContext context) {
-    return Expanded(
-      child: Container(
-        // padding: const EdgeInsets.only(left: 20, top: 0, right: 20),
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20),
-          ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.only(top: 30.0),
-          child: Column(
-            children: [
-              Text(
-                'Add Task',
-                style: TextStyle(
-                  color: kLightBlueBackground,
-                  fontSize: 30,
-                ),
-              ),
-              TextField(
-                autofocus: true,
-                decoration: InputDecoration(
-                  border: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      // color: kLightBlueBackground,
-                      color: Colors.red,
-                      // width: 30,
-                    ),
-                  ),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: kLightBlueBackground,
-                      width: 4.0,
-                    ),
-                  ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.lightBlueAccent,
-                      // color: Colors.red,
-                      width: 6.0,
-                    ),
-                  ),
-                ),
-                style: TextStyle(),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 30.0),
-                child: Material(
-                  color: kLightBlueBackground,
-                  // borderRadius: BorderRadius.circular(12.0),
-                  elevation: 5.0,
-                  child: MaterialButton(
-                    onPressed: () {},
-                    // minWidth: 300.0,
-                    minWidth: double.infinity,
-                    height: 42.0,
-                    child: Text(
-                      'Add',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18.0,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
     );
   }
 
@@ -175,11 +97,11 @@ class TasksScreen extends StatelessWidget {
           size: 40,
         ),
         onPressed: () {
-          //
           showModalBottomSheet(
             backgroundColor: Colors.transparent,
+            isScrollControlled: true,
             context: context,
-            builder: buildModalBottomSheet,
+            builder: (context) => AddTaskScreen(),
           );
         },
       ),
